8436dbcc05e2d38391f703b19c1493bd524bbbc2
Kateryna Purchel <kateryna@ardanis.com>
2018-05-30 22:03:29 +0100

    notable_django

--- hello/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Places(models.Model): 
-    name = models.CharField('name',max_length=255, blank=False, unique=True)
-    placeId = models.CharField('place id',max_length=255, blank=False, unique=True)
-    date_created = models.DateTimeField('date created', auto_now_add=True)
-    date_modified = models.DateTimeField('date modified',auto_now=True)
